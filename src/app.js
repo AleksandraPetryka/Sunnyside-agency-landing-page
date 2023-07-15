731397cd2ba61dfa317c5cd6bf81fb33dd16a015
AleksandraPetryka <aleksandra.petryka@gmail.com>
2023-07-15 14:53:55 +0200

    add interactive menu in mobile

---
+++ src/app.js
@@ -0,0 +1,12 @@
+function changeMenuVisibility() {
+  let menuItems = document.getElementById("menu-items");
+  if (menuItems.style.display === "") {
+    menuItems.style.display = "flex";
+  }
+  else if (menuItems.style.display === "none") {
+    menuItems.style.display = "flex";
+  }
+  else {
+    menuItems.style.display = "none";
+  }
+}
