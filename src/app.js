e82eebc5bcc41b506cc656d219b32ed6d85842e6
AleksandraPetryka <aleksandra.petryka@gmail.com>
2023-08-07 13:36:58 +0200

    cleanup

--- src/app.js
+++ src/app.js
@@ -2,11 +2,9 @@ function changeMenuVisibility() {
   let menuItems = document.getElementById("menu-items");
   if (menuItems.style.display === "") {
     menuItems.style.display = "flex";
-  }
-  else if (menuItems.style.display === "none") {
+  } else if (menuItems.style.display === "none") {
     menuItems.style.display = "flex";
-  }
-  else {
+  } else {
     menuItems.style.display = "none";
   }
 }
